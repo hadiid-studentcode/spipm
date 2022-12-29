27be7c1494fe5b79911e272778f0ffc5029acb85
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2022-12-29 17:38:06 +0700

    assets

--- resources/views/Login/index.blade.php
+++ resources/views/Login/index.blade.php
@@ -6,29 +6,97 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sistem Perkaderan IPM | SPIPM </title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="Assets/plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="Assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="Assets/dist/css/adminlte.min.css">
 </head>
 
-<body>
-    <h1>Halaman Login</h1>
-
-    @if (session()->has('error'))
-       
-            <strong> {{ session('error') }}</strong>
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="../../index2.html"><b>SP</b>IPM</a>
+            <img src="" class="img-fluid" alt="...">
         </div>
-    @endif
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="/login" method="post">
-        @csrf
+                @if (session()->has('error'))
+                    <strong> {{ session('error') }}</strong>
+            </div>
+            @endif
 
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username"> <br>
+            <form action="/login" method="post">
+                @csrf
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Username" name="username">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-user"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" placeholder="Password" name="password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    {{-- <div class="col-8">
+                        <div class="icheck-primary">
+                            <input type="checkbox" id="remember">
+                            <label for="remember">
+                                Remember Me
+                            </label>
+                        </div>
+                    </div> --}}
+                    <!-- /.col -->
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
 
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password">
+            {{-- <div class="social-auth-links text-center mb-3">
+                <p>- OR -</p>
+                <a href="#" class="btn btn-block btn-primary">
+                    <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+                </a>
+                <a href="#" class="btn btn-block btn-danger">
+                    <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+                </a>
+            </div> --}}
+            <!-- /.social-auth-links -->
 
-        <button type="submit">Masuk</button>
+            <p class="mb-1">
+                <a href="forgot-password.html">I forgot my password</a>
+            </p>
+            {{-- <p class="mb-0">
+                <a href="register.html" class="text-center">Register a new membership</a>
+            </p> --}}
+        </div>
+        <!-- /.login-card-body -->
+    </div>
+    </div>
+    <!-- /.login-box -->
 
-    </form>
+    <!-- jQuery -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
