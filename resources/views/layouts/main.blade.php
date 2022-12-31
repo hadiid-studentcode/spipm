ab26acd10070d7bf28da11727301164d520c7095
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2022-12-31 15:31:17 +0700

    panitia

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -156,7 +156,7 @@
                 "responsive": true,
                 "lengthChange": false,
                 "autoWidth": false,
-                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+                "buttons": ["excel", "pdf", "print"]
             }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
             $('#example2').DataTable({
                 "paging": true,
